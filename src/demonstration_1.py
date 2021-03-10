9ad8f89301cdaa7f4cf76f09fe20a10b1bc23226
Anna Hangstefer <anna-hangstefer@lambdastudents.com>
2021-03-09 21:27:29 -0500

    tree

--- src/demonstration_1.py
+++ src/demonstration_1.py
@@ -23,7 +23,50 @@ class BinaryTreeNode:
         self.value = value
         self.left = left
         self.right = right
+    
+    def insert(self, value):
+        new_node = BinaryTreeNode(value)   #O(1)
+        if value < self.value:       #O(1)
+            #insert to the left
+            if self.left is None:     #O(1)
+                self.left = new_node
+            else:
+                self.left.insert(value)
+        else:
+            #insert to the right
+            if self.right is None:
+                self.right = new_node
+            else:
+                self.right.insert(value)
 
-def maxDepth(self, root):
-    # Your code here
+    def maxDepth(self):
+        # solve case w/ no children
+        if self.left is None and self.right is None:
+            return 1
+
+        left_height = 0
+        right_height = 0
+        #solve case with children
+        #How far can I go on the left and the right? (check both sides)
+        if self.left is not None:
+            left_height = self.left.maxDepth()
+        if self.right is not None:
+            right_height = self.right.maxDepth()
+        
+        max_depth = max(left_height, right_height)
+        return max_depth + 1
+         
+
+
+root = BinaryTreeNode(8)
+
+root.insert(5)
+root.insert(11)
+root.insert(2)
+root.insert(7)
+root.insert(10)
+root.insert(12)  
+
+print(root.maxDepth())
+    
 
